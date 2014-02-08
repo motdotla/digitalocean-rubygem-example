555d69358af8845ebe7e0dafd8ef76cfe54dc597
scottmotte <scott@scottmotte.com>
2014-02-08 01:14:50 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'digitalocean', '1.0.0.rc.2'
+gem "dotenv", "~> 0.9.0"
---
+++ digitalocean-rubygem-example.rb
@@ -0,0 +1,12 @@
+require 'rubygems' 
+require 'bundler/setup'
+
+require 'dotenv'
+Dotenv.load
+
+require 'digitalocean'
+Digitalocean.client_id  = "your_client_id"
+Digitalocean.api_key    = "your_api_key"
+
+regions = Digitalocean::Region.all
+puts regions.inspect
